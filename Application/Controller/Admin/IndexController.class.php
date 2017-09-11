bc1c31feeb9dea06be026859342d2769ff085fb7
admin <969060233@qq.com>
2017-09-11 22:48:23 +0800

    controller

---
+++ Application/Controller/Admin/IndexController.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/11
+ */
+class IndexController extends Controller
+{
+     public function index(){
+         echo "我是首页";
+     }
+}
