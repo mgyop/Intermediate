9836c863292ec4c8b0fb629307385d9cbf324f8c
huangchao1234567 <863080579@qq.com>
2017-09-12 15:11:54 +0800

    修改后台页面显示

--- Application/Controller/Admin/IndexController.class.php
+++ Application/Controller/Admin/IndexController.class.php
@@ -20,4 +20,36 @@ class IndexController extends Controller
     public function main(){
         $this->display('main');
     }
+    public function order(){
+        $this->display('order');
+    }
+    public function admin_list(){
+        $this->display('admin_list');
+    }
+    public function revise_password(){
+    $this->display('revise_password');
+    }
+
+    public function user_list(){
+        $this->display('user_list');
+    }
+    public function add_user(){
+        $this->display('add_user');
+    }
+    public function user_rank(){
+        $this->display('user_rank');
+    }
+    public function user_message(){
+        $this->display('user_message');
+    }
+    public function advertising_list(){
+        $this->display('advertising_list');
+    }
+    public function basic_settings(){
+        $this->display('basic_settings');
+    }
+    public function advertising(){
+        $this->display('advertising');
+    }
+
 }
