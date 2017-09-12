a47f8eb8d3bdb0510054ffb1243b657756fc59be
huangchao1234567 <863080579@qq.com>
2017-09-12 14:12:09 +0800

    验证登陆完成和后台页面的html拷入

--- Application/Controller/Admin/IndexController.class.php
+++ Application/Controller/Admin/IndexController.class.php
@@ -1,14 +1,14 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2017/9/11
- */
+
 class IndexController extends Controller
-{
+    {/**
+     * Class IndexController
+     *后台首页
+     */
      public function index(){
          echo "我是首页";
          $MembersModel = D('members');//new MembersModel();
+         $MembersModel->index();
      }
 }
