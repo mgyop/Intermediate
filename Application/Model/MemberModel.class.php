f36d987640bf0b864ebfbd1feaf96aa8b269c8c5
wt <969060233@qq.com>
2017-09-13 02:04:51 +0800

    员工列表 搜索分页

--- Application/Model/MemberModel.class.php
+++ Application/Model/MemberModel.class.php
@@ -6,6 +6,9 @@
  */
 class MemberModel extends Model
 {
+    public $arr_condition = [];  //保存条件
+    private $str_condition = '';  //保存拼接过后的字符串
+    private $where='';
     /**
      * 登录验证
      * @param $post
@@ -81,6 +84,39 @@ class MemberModel extends Model
      */
     public function editLoginIp($sql){
         return $this->db->query($sql);
+    }/**
+ * 查询
+ * @param $data
+ * @return string
+ */
+    public function serach($data){
+//         dump($data);die;
+        //分类id
+        if(!empty($data['group_id'])){
+            $this->arr_condition[] = "group_id='{$data['group_id']}'";
+        }
+
+        //username
+        if(!empty($data['username'])){
+            $this->arr_condition[] = "username='{$data['username']}'";
+        }
+        //status
+        if(!empty($data['realname'])){
+            $this->arr_condition[] = "realname='{$data['realname']}'";
+        }
+        //telephone
+        if(!empty($data['realname'])){
+            $this->arr_condition[] = "telephone='{$data['telephone']}'";
+        }
+        //keyword
+        if(!empty($data['keyword'])){
+            $this->arr_condition[] = "(username like '%{$data['keyword']}%' or realname like '%{$data['keyword']}%')";
+        }
+        $this->str_condition =implode(" and ",$this->arr_condition);
+        if(!empty($this->str_condition)){
+            $this->where = " where ".$this->str_condition;
+        }
+        return $this->where;
     }
 }
 
