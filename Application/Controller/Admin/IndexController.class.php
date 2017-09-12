10c47c2e5559108c3024f835b652217bcd8c2ca3
wt <969060233@qq.com>
2017-09-12 16:13:05 +0800

    index控制器的修改

--- Application/Controller/Admin/IndexController.class.php
+++ Application/Controller/Admin/IndexController.class.php
@@ -11,45 +11,20 @@ class IndexController extends Controller
         //echo "后台首页";
         $this->display('index');
     }
-    public function bar(){
-        $this->display('bar');
+
+    /**
+     * 菜单栏
+     */
+    public function menu(){
+        $this->display('menu');
     }
+    //顶部logo
     public function top(){
         $this->display('top');
     }
-    public function main(){
+    //页面主体
+    public function main()
+    {
         $this->display('main');
     }
-    public function order(){
-        $this->display('order');
-    }
-    public function admin_list(){
-        $this->display('admin_list');
-    }
-    public function revise_password(){
-    $this->display('revise_password');
-    }
-
-    public function user_list(){
-        $this->display('user_list');
-    }
-    public function add_user(){
-        $this->display('add_user');
-    }
-    public function user_rank(){
-        $this->display('user_rank');
-    }
-    public function user_message(){
-        $this->display('user_message');
-    }
-    public function advertising_list(){
-        $this->display('advertising_list');
-    }
-    public function basic_settings(){
-        $this->display('basic_settings');
-    }
-    public function advertising(){
-        $this->display('advertising');
-    }
-
 }
