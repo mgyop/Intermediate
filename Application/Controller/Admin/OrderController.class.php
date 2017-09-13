34cc375420138ec88d4ca793b5fc73b408241f4c
huangchao1234567 <863080579@qq.com>
2017-09-13 19:12:47 +0800

    预约状态的操作

--- Application/Controller/Admin/OrderController.class.php
+++ Application/Controller/Admin/OrderController.class.php
@@ -1,4 +1,5 @@
 <?php
+
 class OrderController extends Controller
 {
 
@@ -6,16 +7,74 @@ class OrderController extends Controller
      * 处理预约状态
      */
 
-    public function select(){
-      //查询出所有未处理的状态
-
-      $orderModel=D("order");
-       $order= $orderModel->select();
-      // var_dump($order);die;
-       //回显到表单
-        $this->assign("order",$order);
+    public function select()
+    {
+        //查询出所有未处理的状态
+        $orderModel = D("order");
+        $order = $orderModel->select();
+        // var_dump($order);die;
+        //回显到表单
+        $this->assign("order", $order);
         //页面显示
         $this->display("order");
+    }
+
+    /**
+     * 处理状态,是否同意
+     */
+    public function update()
+    {
+        //接收数据
+        $roder_id = $_GET['order_id'];
+        $ty = $_GET['ty'];//ty==1时同意ty==0时不同意同意是自定义的
+        //var_dump($ty);die;
+        //处理数据
+        if ($ty == 1) {
+            $orderModer = D("order");
+            $row = $orderModer->update($roder_id, $ty);
+            // var_dump($row);die;
+            //页面显示
+            $this->success("index.php?p=Admin&c=Order&a=select");
+        }
+
+    }
+    public function update_no(){
+        $roder_id = $_GET['order_id'];
+        $ty = $_GET['ty'];//ty==1时同意ty==2时不同意同意是自定义的
+        //var_dump($_GET);die;
+        if($ty == 2) {
+            //不同意的处理
+            $orderModer = D("order");
+            $row = $orderModer->update_no($roder_id,$ty);
+            //var_dump($row);die;
+            $this->success("index.php?p=Admin&c=Order&a=select");
+        }
+    }
+    /**
+     * 回复操作
+     */
+    public function insert(){
+       // var_dump($_SERVER);
+        if ($_SERVER['REQUEST_METHOD']=='GET'){
+            //显示回复表框
+            //接收id
+            $roder_id = $_GET['order_id'];
+            //显示回复表单
+            $this->assign('roder_id',$roder_id);
+            $this->display("insert");
+        }else{
+            //接收数据
+            $order_id=$_POST['roder_id'];
+            $reply=$_POST['reply'];
+            //var_dump($post);die;
+            //处理数据,即添加回复数据
+            $orderModer = D("order");
+            $orderModer->reply($order_id,$reply);
+            //页面显示
+            $this->success("index.php?p=Admin&c=Order&a=select");
+        }
+
+
     }
 
 }
--- Application/Model/OrderModel.class.php
+++ Application/Model/OrderModel.class.php
@@ -5,7 +5,8 @@ class OrderModel extends Model
      * @return array|null|void
      * 显示预约表单
      */
-    public function order(){
+    public function order()
+    {
         return $this->getAll();
     }
 
@@ -14,17 +15,18 @@ class OrderModel extends Model
      * @return bool
      * 预约功能
      */
-    public function insert($post){
-            $date=strtotime($post['date']);
-        $sql="insert into `order` set phone='{$post['phone']}',
+    public function insert($post)
+    {
+        $date = strtotime($post['date']);
+        $sql = "insert into `order` set phone='{$post['phone']}',
                 realname='{$post['realname']}',
                 barber='{$post['barber']}',
                 content='{$post['content']}',
                 date='{$date}'";
-        $rows= $this->db->query($sql);
+        $rows = $this->db->query($sql);
         //var_dump($sql);
         //die;
-        if ($rows==false){
+        if ($rows == false) {
             return false;
         }
         return true;
@@ -34,24 +36,27 @@ class OrderModel extends Model
      * @return array|bool|null|void
      * 查询员工数据
      */
-    public function slect_member(){
-        $sql="select * from member";
-        $member= $this->db->fetchAll($sql);
-       //var_dump($rows);die;
-        if ($member==false){
+    public function slect_member()
+    {
+        $sql = "select * from member";
+        $member = $this->db->fetchAll($sql);
+        //var_dump($rows);die;
+        if ($member == false) {
             return false;
         }
         return $member;//返回所有员工的数据
     }
+
     /**
      * 根据员工id查询员工名字
      */
 
-    public function member_id($member2){
-        $sql="select * from member where member_id='{'$member2'}'";
-        $member= $this->db->fetchRow($sql);
+    public function member_id($member2)
+    {
+        $sql = "select * from member where member_id='{'$member2'}'";
+        $member = $this->db->fetchRow($sql);
         //var_dump($rows);die;
-        if ($member==false){
+        if ($member == false) {
             return false;
         }
         return $member;//根据id查询员工数据
@@ -60,16 +65,64 @@ class OrderModel extends Model
     /**
      * 后台预约查询所有数据
      */
-    public function select(){
+    public function select()
+    {
 
-        $sql="select * from `order` where status=3";
-        $order= $this->db->fetchAll($sql);
+        $sql = "select * from `order` where status=3";
+        $order = $this->db->fetchAll($sql);
         //var_dump($order);die;
-        if ($order==false){//3表示未处理状态 1成功2失败
-        return false;//查询出所有未处理的数据
-        }else{
+        if ($order == false) {//3表示未处理状态 1成功2失败
+            return false;//查询出所有未处理的数据
+        } else {
             return $order;
         }
+    }
+
+    /**
+     * @param $roder_id
+     * 根据order_id处理状态
+     */
+    public function update($roder_id, $ty)
+    {
+        if ($ty == 1) {
+            //这是同意的时候处理
+            $sql = "update `order` set status=1 where order_id='{$roder_id}'";
+            $rows = $this->db->query($sql);
+            if ($rows == false) {
+                return false;
+            } else {
+                return $rows;
+            }
+
+        }
+    }
+    public function update_no($roder_id, $ty){
+
+        if ($ty == 2) {
+            //这是不同意的时候处理
+            $sql = "update `order` set status=2 where order_id='{$roder_id}'";
+            $rows = $this->db->query($sql);
+            if ($rows == false) {
+                return false;
+            } else {
+                return $rows;
+            }
+    }
+
 
     }
+    /**
+     * 将回复内容添加到数据库
+     */
+    public function reply($order_id,$reply){
+        //var_dump($roder_id,$reply);die;
+        $sql="update `order` set reply='{$reply}' where order_id='{$order_id}'";
+        $this->db->query($sql);
+    }
+
+
+
+
+
+
 }
