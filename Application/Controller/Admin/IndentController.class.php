71b6f133e0f7430750ce58246840c3704042cb0e
huangchao1234567 <863080579@qq.com>
2017-09-16 00:55:16 +0800

    订单系统

--- Application/Controller/Admin/IndentController.class.php
+++ Application/Controller/Admin/IndentController.class.php
@@ -6,22 +6,77 @@
  */
 class IndentController extends Controller
 {
+
+
     /**
      * 查询订单表单数据显示在表单上
      * 显示订单表单
      */
-    public function index(){
-    //1.查询所有数据
-      //创建一个对象帮我们完成
-        $indentModel=D("Indent");
-       $rows= $indentModel->select();
-        $this->assign('rows',$rows);
+    public function index()
+    {
+        $get = $_GET;
+        //var_dump($get);die;
+        //创建一个对象
+        //1.查询所有数据
+        //创建一个对象帮我们完成
+        $page = $_GET['page']??1;
+        $indentModel = D("Indent");
+        $where = $indentModel->index($get);
+        //var_dump($where);die;
+
+        $rows = $indentModel->select($page, $where);
+        //var_dump($rows);
+        if ($rows == false) {
+            $rows['indent'] = [];
+        }
+        $this->assign('rows', $rows);
+        //查询订单所有数据 ,回显到搜索表单
+        $indentMode = D("Indent");
+        $row = $indentMode->select_indent();
+        //var_dump($row);die;
+        $this->assign('indent', $row);
+        /**
+         * 查询会员数据user表
+         */
+        $indentMode = D("Indent");
+        $rowss = $indentMode->select_user();
+
+        foreach ($rowss as $key => $v) {
+            $user_username[$v['user_id']] = $v;
+            unset($v);
+        }
+        //var_dump($user_username);die;
+        $this->assign('user', $user_username);
+        // var_dump($rowss);die;
+
+        /**
+         * 查询商品所有数据
+         */
+        $indentMode = D("Indent");
+        $goods = $indentMode->select_goods();
+        //var_dump($goods);die;
+        foreach ($goods as $key => $val) {
+            $goods[$val['goods_id']] = $val;
+            unset($val);
+        }
+        //var_dump($goods);die;
+        $this->assign("goods", $goods);
+        $this->display("index");
+    }
+
+
+    public function send()
+    {
+
+        //接收数据
+        $goodsordr_id = $_GET['goodsordr_id'];
+        // var_dump($goodsordr_id);
+        $indentMode = new IndentModel();
+        $indentMode->update_send($goodsordr_id);
+        //var_dump($rows);die;
         $this->display("index");
 
-
-
-
-
     }
+
 
 }
--- Application/Model/IndentModel.class.php
+++ Application/Model/IndentModel.class.php
@@ -5,15 +5,110 @@
  * 订单系统
  */
 class IndentModel extends Model
-{
+{   private $data=[];//将条件放入数组中
+    private $dent='';//将以为数组转化为字符串放入数组中
+    private $where="";
 
-    public function select()
+    public function select($page,$where)
     {
-       $sql="select * from goodsorder";
-       $rows=$this->db->fetchAll($sql);
-       //var_dump($rows);die;
-        return $rows;
+        //查询总条数
+        $y=3;
+        //计算出总的条数
+        $sql2="select count(*) from goodsorder $where";
+        $zongtiao=$this->db->fetchColumn($sql2);//总条数
+        $yeshu=ceil($zongtiao/$y);  //计算出总的页数
+        //dump($yeshu);die;
+        $page<1?1:$page;
+        $page>$yeshu?$yeshu:$page;
+        $x=($page-1)*$y;
 
+        $page_s=($page-1)<1?1:($page-1);
+        $page_x=($page+1)>$yeshu?$page:($page+1);
+
+        $sql = "select * from goodsorder $where limit $x,$y";
+        $indent = $this->db->fetchAll($sql);
+        //var_dump($order);die;
+        if ($indent==false){
+            $this->error="条件不存在";
+            return false;
+        }else {
+            return ["page"=>$page,"yeshu"=>$yeshu,"zongtiao"=>$zongtiao,"indent"=>$indent,
+                "page_s"=>$page_s,"page_x"=>$page_x
+            ];
+        }
+    }
+    public function index($get){
+        /**
+         * 拼装搜索条件
+         */
+        //var_dump($get);die;
+
+        if (!empty($get['is_pay'])){
+            $this->data[]="is_pay='{$get['is_pay']}'";
+        }
+        if (!empty($get['addr'])){
+            $this->data[]="addr='{$get['addr']}'";
+        }
+
+        if(!empty($get['keyword'])){
+            $this->data[]="(addr like '%{$get['keyword']}%' or order_num like '%{$get['keyword']}%')";
+        }
+        //将一维数组转化为字符串
+        $this->dent=implode(" and ",$this->data);
+
+        if (!empty($this->dent)){
+            $this->where=" where ".$this->dent;
+        }
+       return $this->where;
 
     }
+
+    /**
+     * @return array
+     * 查询订单的数据
+     */
+    public function select_indent(){
+        $sql="select * from goodsorder";
+        $rows=$this->db->fetchAll($sql);
+        if ($rows==false){
+            return false;
+        }else{
+            return $rows;
+        }
+    }
+    /**
+     * 查询会员表
+     */
+    public function select_user(){
+        $sql="select * from `user`";
+        $rowss=$this->db->fetchAll($sql);
+       // var_dump($rowss);
+            return $rowss;
+    }
+    /**
+     * 查询商品数据
+     */
+
+    public function select_goods(){
+        $sql="select * from goods";
+        $goods=$this->db->fetchAll($sql);
+        // var_dump($rowss);
+        return $goods;
+    }
+
+    /**
+     * 对发货进行操作
+     */
+    public function update_send($goodsordr_id){
+        $sql="select * from goodsorder where goodsordr_id='{$goodsordr_id}'";
+        $row=$this->db->fetchRow($sql);
+        $row['is_send']=1;
+
+
+      $sql="update goodsorder set is_send='{$row['is_send']}' where goodsordr_id='{$goodsordr_id}'";
+     $rows=$this->db->query($sql);
+    return $rows;
+    }
+
+
 }
